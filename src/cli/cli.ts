a3b41654e176b0bc33050a1bb9c8e8d2ef089338
Jakub Pomykała <jakub.pomykala@gmail.com>
2022-12-09 18:26:05 +0100

    Init source code

---
+++ src/cli/cli.ts
@@ -0,0 +1,155 @@
+"use strict";
+import stream from "node:stream";
+import { bold, greenBright } from "colorette";
+
+const axios = require("axios");
+const fs = require("fs");
+const glob = require("glob");
+const path = require("path");
+const yargs = require("yargs/yargs");
+const { hideBin } = require("yargs/helpers");
+
+const argv = yargs(hideBin(process.argv))
+  .required("apiKey", "API Key is required")
+  .required("template", "Template is required")
+  .boolean("no-cache")
+  .boolean("overwrite")
+  .boolean("debug").argv;
+
+const EXTENSIONS = [
+  "png",
+  "jpg",
+  "jpeg",
+  "pdf",
+  "webp",
+  "gif",
+  "bmp",
+  "tiff",
+  "eps",
+];
+
+const execute = async () => {
+  console.log(bold("RenderForm CLI working..."));
+  const directory = argv._[argv._.length - 1];
+  if (!directory) {
+    console.log("No directory provided");
+    return;
+  }
+
+  if (argv.debug) {
+    console.log(bold("Directory: ") + path.resolve(directory));
+  }
+  const filePaths = glob.sync(directory + "/**/*.json");
+
+  for (const filePath of filePaths) {
+    await processJsonFile(filePath);
+  }
+};
+
+const processJsonFile = async (filePath: string) => {
+  const fileRaw = fs.readFileSync(filePath, "utf8");
+  const jsonFile = JSON.parse(fileRaw);
+
+  const isRenderFormFile = jsonFile.hasOwnProperty("renderform");
+  if (!isRenderFormFile) {
+    return;
+  }
+  const jsonFileName = path.parse(filePath).name;
+  const fileDirectory = path.parse(filePath).dir;
+  const canOverwrite = argv.overwrite;
+  if (!canOverwrite) {
+    for (const extension of EXTENSIONS) {
+      const outputPath = path.join(
+        path.dirname(filePath),
+        jsonFileName + "." + extension
+      );
+      if (fs.existsSync(outputPath)) {
+        console.log(
+          `${bold(
+            "Skipping:"
+          )} ${jsonFileName}.${extension} already exists. Use ${bold(
+            "--overwrite"
+          )} to overwrite existing files.`
+        );
+        return;
+      }
+    }
+  }
+  process.stdout.write(greenBright("Render: ") + filePath);
+  const template = argv.template;
+  const changes = jsonFile.changes;
+
+  if (argv.debug) {
+    console.log("Template: " + template);
+    console.log("Changes: " + JSON.stringify(changes));
+  }
+
+  const imageUrl = await renderImage(template, changes);
+  const imageData = await downloadImage(imageUrl);
+  const imageFileExtension = path.extname(imageUrl);
+  const savePath = fileDirectory + "/" + jsonFileName + imageFileExtension;
+  await saveImage(imageData, savePath);
+  process.stdout.write(" -> " + savePath);
+  console.log("");
+};
+
+const downloadImage = async (imageUrl): Promise<any> => {
+  const responseImage = await axios.get(imageUrl, { responseType: "stream" });
+  return responseImage.data;
+};
+
+const saveImage = async (imageData: stream, outputPath: string) => {
+  return new Promise<void>((resolve, reject) => {
+    imageData
+      .pipe(fs.createWriteStream(outputPath))
+      .on("finish", () => resolve())
+      .on("error", (e) => reject(e));
+  });
+};
+
+const renderImage = async (
+  template: string,
+  changes: object[]
+): Promise<string> => {
+  const requestBody = {
+    template,
+    changes,
+  };
+
+  if (!argv.cache) {
+    requestBody["version"] = Date.now();
+  }
+  const headers = {
+    "content-type": "application/json",
+    "x-api-key": argv.apiKey,
+  };
+
+  if (argv.debug) {
+    console.log("Headers: " + JSON.stringify(headers));
+    console.log("Request Body: " + JSON.stringify(requestBody));
+  }
+
+  const responseRender = await axios.post(
+    "https://get.renderform.io/api/v1/render",
+    requestBody,
+    { headers }
+  );
+
+  if (argv.debug) {
+    console.log("Response: " + JSON.stringify(responseRender.data));
+  }
+
+  return responseRender.data.href;
+};
+
+(async () => {
+  try {
+    await execute();
+    console.log(greenBright("Finished!"));
+    console.log("");
+    process.exit(0);
+  } catch (e) {
+    console.log(e);
+    process.exit(1);
+  }
+})();
